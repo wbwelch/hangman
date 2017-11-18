840440e487d410ff6f4a465e0ffe10784a4e3977
wbwelch <wbwelch@icloud.com>
2017-11-17 23:14:21 -0600

    Basic HTML and CSS in place.

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,1 +1,36 @@
 // JavaScript Document
+
+//.game-photo is class element when inserting new photos
+
+
+
+var answers = {
+    firstAnswer:"Freddy",
+	secondAnswer:"Jason",
+	thirdAnswer:"Michael",
+	fourthAnswer:"Leatherface",
+	fifthAnswer:"Candyman",
+	sixthAnswer:"Hannibal",
+	seventhAnswer:"Norman",
+	eighthAnswer:"Chucky",
+	ninthAnswer:"Pennywise",
+	tenthAnswer:"Cujo",
+	eleventhAnswer:"Pinhead",
+
+	
+	
+  };
+	
+
+
+
+
+		
+		//if/else/if
+		//correct answers
+		$("answers-reveal").append("")
+		//incorrect answers
+		$("used-letters").append("")
+
+
+	
