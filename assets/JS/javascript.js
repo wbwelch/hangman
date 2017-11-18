5411fe6a990d80791c72055decc25f5ebbdd5c3a
wbwelch <wbwelch@icloud.com>
2017-11-18 02:03:32 -0600

    JS Work

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -4,23 +4,51 @@
 
 
 
-var answers = {
-    firstAnswer:"Freddy",
-	secondAnswer:"Jason",
-	thirdAnswer:"Michael",
-	fourthAnswer:"Leatherface",
-	fifthAnswer:"Candyman",
-	sixthAnswer:"Hannibal",
-	seventhAnswer:"Norman",
-	eighthAnswer:"Chucky",
-	ninthAnswer:"Pennywise",
-	tenthAnswer:"Cujo",
-	eleventhAnswer:"Pinhead",
+
+
+function hangmanGame() {
+	letters = [ "abcdefghijklmnopqrstuvwxyz" ];
+	lives = 12;
+	answers = [ "Freddy", "Jason", "Michael", "Leatherface", "Candyman", "Hannibal", "Norman", "Chucky", "Pennywise", "Cujo", "Pinhead" ];
+	messages = {
+            win: 'Winner, winner, chicken dinner!',
+            lose: 'Death!',
+            guessed: ' already guessed, please try again...',
+            validLetter: 'Please enter a letter from A-Z'
+		  };
 
 	
+	lettersGuessed = lettersMatched = '';
+        numLettersMatched = 0;
 	
-  };
+	gameWord = answers[Math.floor(Math.random() * answers.length)];	
+	//console.log(gameWord);
 	
+ 	answerReveal = document.getElementById("answer-revealed");
+	guessRemain = document.getElementById("guess-remain");
+	usedLetters = document.getElementById("used-letters");
+
+
+	man.innerHTML = 'You have ' + lives + ' lives remaining';
+        output.innerHTML = ''
+	
+}
+
+
+//function inAnswers(x,y) {
+	//for(i = 0; i < x.length; i++){
+		//if(answers[i] === y){
+		//return true;
+		//}
+	//}
+	//return false
+//}
+
+//console.log(inAnswers(answers, "Michael"))
+	
+//for (k in answers) {
+	//console.log(answers[k])
+//}
 
 
 
@@ -28,9 +56,9 @@ var answers = {
 		
 		//if/else/if
 		//correct answers
-		$("answers-reveal").append("")
+		//$("answers-reveal").append("")
 		//incorrect answers
-		$("used-letters").append("")
+		//$("used-letters").append("")
 
 
 	
