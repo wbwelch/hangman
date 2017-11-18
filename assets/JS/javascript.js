7c12fd0f505e0c00376344dca54a7efff305e320
wbwelch <wbwelch@icloud.com>
2017-11-18 04:01:05 -0600

    JS work

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -7,7 +7,6 @@
 
 
 function hangmanGame() {
-	letters = [ "abcdefghijklmnopqrstuvwxyz" ];
 	lives = 12;
 	answers = [ "Freddy", "Jason", "Michael", "Leatherface", "Candyman", "Hannibal", "Norman", "Chucky", "Pennywise", "Cujo", "Pinhead" ];
 	messages = {
@@ -16,24 +15,67 @@ function hangmanGame() {
             guessed: ' already guessed, please try again...',
             validLetter: 'Please enter a letter from A-Z'
 		  };
-
 	
-	lettersGuessed = lettersMatched = '';
-        numLettersMatched = 0;
+
 	
 	gameWord = answers[Math.floor(Math.random() * answers.length)];	
-	//console.log(gameWord);
+
 	
- 	answerReveal = document.getElementById("answer-revealed");
-	guessRemain = document.getElementById("guess-remain");
-	usedLetters = document.getElementById("used-letters");
+	for (i = 0; i <= gameWord.length; i++) {
+		var letterBox = $("<div>");
+		letterBox.addClass("letter-box");
+		$("answer-revealed").append(letterBox);
+	}
+		//console.log(gameWord.length)
 
 
-	man.innerHTML = 'You have ' + lives + ' lives remaining';
-        output.innerHTML = ''
 	
-}
+	$(document).keypress(function(e) {
+    var userGuess = String.fromCharCode(e.which);
+	for (k = 0; k <= gameWord.length; k++) {
+			//console.log(userGuess)
+		if (userGuess.match(gameWord[k])){
+				var letterBtn = $("<button>");
+				letterBtn.addClass("letters");
+				letterBtn.attr("data-letter"), gameWord[k];
+				letterBtn.text(gameWord[k]);
+				$("answer-reveal").append(letterBtn);
+			}
+		else  if (userGuess !== gameWord[k]) {
+				letterBtn2 = $("<button>");
+				letterBtn2.addClass("letters");
+				letterBtn2.attr("data-letter"), gameWord[k];
+				letterBtn2.text(gameWord[k]);;
+				$("used-letters").append(letterBtn2);
 
+			}
+	}
+
+	});
+	
+	
+	var wins = 0
+	//for each win, wins++
+	$("wins-display").html(wins);
+	//console.log(wins);
+ }
+ 
+		
+		
+	//loop until word value == user var value
+		//	for gameWord(k = 0; k < gameword.length; k++) {
+
+	
+
+
+$(document).keydown(hangmanGame)
+
+	
+//$(document).keypress(function(e) {
+    //var s = String.fromCharCode(e.which);
+  //  if (s.match(/[a-zA-Z\.]/))
+      //  console.log(s + ' is a match!');
+//});
 
 //function inAnswers(x,y) {
 	//for(i = 0; i < x.length; i++){
