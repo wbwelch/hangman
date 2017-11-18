ca2737686375cfa19320f1ea4e2a4a24979fe86b
wbwelch <wbwelch@icloud.com>
2017-11-18 07:36:46 -0600

    Js update

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -3,73 +3,105 @@
 //.game-photo is class element when inserting new photos
 
 
-
-
-
-function hangmanGame() {
-	lives = 12;
-	answers = [ "Freddy", "Jason", "Michael", "Leatherface", "Candyman", "Hannibal", "Norman", "Chucky", "Pennywise", "Cujo", "Pinhead" ];
-	messages = {
+var hangmanGame = {
+	letters: ["abcdefghijklmnopqrstuvwxyz"],
+	lives: 12,
+	answers: [ "Freddy", "Jason", "Michael", "Leatherface", "Candyman", "Hannibal", "Norman", "Chucky", "Pennywise", "Cujo", "Pinhead" ],
+	wins: 0,
+	messages: {
             win: 'Winner, winner, chicken dinner!',
             lose: 'Death!',
             guessed: ' already guessed, please try again...',
             validLetter: 'Please enter a letter from A-Z'
-		  };
+		  },
+	
+	
+		
 	
 
+		
+		};
+		
+
+
+	var gameWord = hangmanGame.answers[Math.floor(Math.random() * hangmanGame.answers.length)];
+	console.log("test")
+	console.log(gameWord),
+	console.log(gameWord.length);	
+	console.log(hangmanGame.wins);
+	console.log(hangmanGame.lives);
 	
-	gameWord = answers[Math.floor(Math.random() * answers.length)];	
-		console.log(gameWord)
 
+
+
+$("#guess-remain").text(hangmanGame.lives);
+$("#wins-display").text(hangmanGame.wins);
+
+//if/else/if
+		//if userinput is in gameWord and letters
+
+		//correct answers
+		//$("answers-reveal").append("")
+
+		//incorrect answers
+		//$("used-letters").append("")
 	
-	for (i = 0; i < gameWord.length; i++) {
-		//console.log(gameWord[i]);
-		var letterBox = $("<div>");
-		letterBox.addClass("letter-box");
-		letterBox.text(" ");
-		$("#answer-reveal").append(letterBox);
-	}
-		console.log(gameWord.length)
+	function fullGame(e)	{
+		var pressedKey;
+			if (document.all)   { e = window.event; }
+			if (document.layers || e.which) { pressedKey = e.which; }
+			pressedCharacter = String.fromCharCode(pressedKey).toLowerCase();
+			move(pressedCharacter);
 
-
-	
-	$(document).keypress(function(e) {
-    var userGuess = String.fromCharCode(e.which);
-		for (k = 0; k <= gameWord.length; k++) {
-			//console.log(userGuess)
-		if (userGuess.match(gameWord[k])){
-				var letterBtn = $("<div class='letters'></div>");
-				letterBtn.attr("data-letter"), gameWord[k];
-				letterBtn.text(gameWord[k]);
-				$("#answer-reveal").append(letterBtn);
-			}
-		else  if (userGuess !== gameWord[k]) {
-				letterBtn2 = $("<button>");
-				letterBtn2.addClass("letters");
-				letterBtn2.attr("data-letter"), gameWord[k];
-				letterBtn2.text(gameWord[k]);;
-				$("#used-letters").append(letterBtn2);
-
-			}
 		}
-	});
 
-	
-	
-	
-	var wins = 0
+		
+		
+		for (i = 0; i < gameWord.length; i++) {
+			var letterBox = $("<div>");
+			console.log(gameWord[i]);
+			letterBox.addClass("letter-box");
+			letterBox.text(" ");
+			$("#answer-reveal").append(letterBox);
+
+			for (k = 0; k < gameWord.length; k++) {
+				//if onkeydown === gameWord[k]){
+					//create div in #answer-reveal
+					//add class
+					//add text
+					//append
+					var letterBox = $("<div>");
+					console.log(gameWord[k]);
+					letterBox.addClass("letter-box");
+					letterBox.text(gameWord[k]);
+					$("#answer-reveal").append(letterBox);
+				}
+
+				//else if (pressedKey === hangmanGame.letters && pressedKey !== gameWord[k]) { 
+					//create div in #used-letters
+					//add class
+					//add text
+					//append
+					console.log(gameWord[k]);
+					letterBox.addClass("letter-box");
+					letterBox.text(hangmanGame.letter);
+					$("#answer-reveal").append(letterBox);
+				}
+		//	};
+
+
+		//};
+//	};
 	//for each win, wins++
-	$("wins-display").text(wins);
-	//console.log(wins);
- };
- 
-		
-		
-	//loop until word value == user var value, then reset
-		//	for gameWord(k = 0; k < gameword.length; k++) {
 
 
-$(document).keydown(hangmanGame)
+uniCharCode(onkeydown)
+	
+	//loop until gameWord value == user var value, then reset
+	//	for gameWord(k = 0; k < gameword.length; k++) {
+
+
+
 
 	
 //$(document).keypress(function(e) {
@@ -86,8 +118,6 @@ $(document).keydown(hangmanGame)
 	//}
 	//return false
 //}
-
-//console.log(inAnswers(answers, "Michael"))
 	
 //for (k in answers) {
 	//console.log(answers[k])
@@ -96,12 +126,7 @@ $(document).keydown(hangmanGame)
 
 
 
-		
-		//if/else/if
-		//correct answers
-		//$("answers-reveal").append("")
-		//incorrect answers
-		//$("used-letters").append("")
+	
 
 
 	
