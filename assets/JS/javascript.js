ea0102fad20c7fe3b19df82e770a56994654a84e
wbwelch <wbwelch@icloud.com>
2017-11-18 14:47:14 -0600

    Psuedocode cleaned up

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,7 +1,6 @@
 // JavaScript Document
 
-//.game-photo is class element when inserting new photos
-
+//create object with letter, lives, answer arrays, wins, message
 
 var hangmanGame = {
 	letters: ["abcdefghijklmnopqrstuvwxyz"],
@@ -14,17 +13,13 @@ var hangmanGame = {
             guessed: ' already guessed, please try again...',
             validLetter: 'Please enter a letter from A-Z'
 		  },
-	
-	
-		
-	
-
-		
 		};
 		
 
-
+	//generate random word
 	var gameWord = hangmanGame.answers[Math.floor(Math.random() * hangmanGame.answers.length)];
+
+	//console tests
 	console.log("test")
 	console.log(gameWord),
 	console.log(gameWord.length);	
@@ -32,99 +27,76 @@ var hangmanGame = {
 	console.log(hangmanGame.lives);
 	
 
-
+//Why isn't the text jquery working?
 
 $("#guess-remain").text(hangmanGame.lives);
 $("#wins-display").text(hangmanGame.wins);
 
-//if/else/if
-		//if userinput is in gameWord and letters
 
-		//correct answers
-		//$("answers-reveal").append("")
-
-		//incorrect answers
-		//$("used-letters").append("")
 	
-	function fullGame(e)	{
-		var pressedKey;
-			if (document.all)   { e = window.event; }
-			if (document.layers || e.which) { pressedKey = e.which; }
-			pressedCharacter = String.fromCharCode(pressedKey).toLowerCase();
-			move(pressedCharacter);
+//create game function
 
-		}
 
-		
-		
+		//generate underlines the length of gameWord
+		//why did this stop working?
 		for (i = 0; i < gameWord.length; i++) {
 			var letterBox = $("<div>");
 			console.log(gameWord[i]);
 			letterBox.addClass("letter-box");
 			letterBox.text(" ");
 			$("#answer-reveal").append(letterBox);
+			
+		//correct guesses
+		//$("answers-reveal").append("")
 
+		//incorrect guesses
+		//$("used-letters").append("")
+
+		//how do i compare user input to a value of a property in an object
+			//need .toLowerCase user input for accuracy
 			for (k = 0; k < gameWord.length; k++) {
 				//if onkeydown === gameWord[k]){
 					//create div in #answer-reveal
 					//add class
 					//add text
 					//append
-					var letterBox = $("<div>");
-					console.log(gameWord[k]);
-					letterBox.addClass("letter-box");
-					letterBox.text(gameWord[k]);
-					$("#answer-reveal").append(letterBox);
-				}
+				//can i add text to the existing created divs with the bottom borders?
+				
+				
 
-				//else if (pressedKey === hangmanGame.letters && pressedKey !== gameWord[k]) { 
+				//else if onkeydown is !== letters && onkeydown !==gameWord 
 					//create div in #used-letters
 					//add class
 					//add text
 					//append
-					console.log(gameWord[k]);
-					letterBox.addClass("letter-box");
-					letterBox.text(hangmanGame.letter);
-					$("#answer-reveal").append(letterBox);
-				}
-		//	};
+					//lives--
+				
+				//else if already guessed (compare to letters in guessed word area), do nothing
+					//creat a temporary variable...change the index value?
+					//max length is the length of the alphabet?
+					
+			};
 
-
-		//};
-//	};
-	//for each win, wins++
-
-
-uniCharCode(onkeydown)
+	//for each win, ++wins
 	
-	//loop until gameWord value == user var value, then reset
-	//	for gameWord(k = 0; k < gameword.length; k++) {
+	//loop until user var value == gameWord value or lives value <1
+			
+	//display message
+
+	//change image
+	
+	//play song
+		
+	//reset game(loop)
+			
+	//initiate game on keypress(call function)
+
+			
 
 
 
 
 	
-//$(document).keypress(function(e) {
-    //var s = String.fromCharCode(e.which);
-  //  if (s.match(/[a-zA-Z\.]/))
-      //  console.log(s + ' is a match!');
-//});
-
-//function inAnswers(x,y) {
-	//for(i = 0; i < x.length; i++){
-		//if(answers[i] === y){
-		//return true;
-		//}
-	//}
-	//return false
-//}
-	
-//for (k in answers) {
-	//console.log(answers[k])
-//}
-
-
-
 
 	
 
