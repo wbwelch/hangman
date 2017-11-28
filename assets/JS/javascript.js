bd813d8993146c1f27a7edbdf4e44581b5aaaae4
wbwelch <wbwelch@icloud.com>
2017-11-28 12:36:58 -0600

    JS clean, last letter guessed defined, psuedocode completed

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -15,16 +15,11 @@ $(document).ready(function() {
 				character: 'Please enter a valid letter'
 			  },
 
-
-
-
 	};
 
 		//generate random word
 		var gameWord = hangmanGame.answers[Math.floor(Math.random() * hangmanGame.answers.length)];
 	 	gameWord.split()
-
-
 
 		//console tests
 		console.log("test");
@@ -39,81 +34,76 @@ $(document).ready(function() {
 		$("#wins-display").append("<p>" + wins + "</p>");
 		$("#guess-remain").html("<p>" + lives + "</p>");
 
-
 		//generate underlines the length of gameWord
 		$("html").one("keyup", function() {
 			for (i = 0; i < gameWord.length; i++) {
 				var letterBox = $("<div class='letter-box'></div>");
 				console.log(gameWord[i]);
-				letterBox.text(gameWord[i]);
+			//	letterBox.text(gameWord[i]); //use when time to write letters to screen
 				$("#answer-reveal").append(letterBox);
-				$(".letter-box").css('color', 'rgba(77, 72, 72, 0');
+			//	$(".letter-box").css('color', 'rgba(77, 72, 72, 0');//transparent letters
 
 			}
 		});
-	
-	//(why is guessString = 0)
 
-
-	var guessStr = hangmanGame.userGuesses.length;
-	var lastGuess = guessStr.length - 1;
-	console.log(lastGuess);
-
-
-
-	//loop through letters to verify valid entry
-
+		var userG = hangmanGame.userGuesses
+		
+		document.onkeypress = function(keyPressed) {
+			var keyPressed = keyPressed || window.event;
+			charCode = keyPressed.keyCode || keyPressed.which;
+			lettersGuessed = String.fromCharCode(charCode);
+		
+			console.log(lettersGuessed)};
+			//loop through letters to verify entry is in letter array
+			document.onkeydown = function(input) {
+				for (i = 0; i < hangmanGame.userGuesses.length; i++) {
+					if (lettersGuessed == i) {
+						console.log("hi");//not working, once returns false, moves on; 		how do I get it to cycle every index in array and look for 		any match?
+						//continue
+					} else if (lettersGuessed !== i){
+						console.log("Nope");
+						//display character message
+					};
+				};
+			};
+			
 		document.onkeyup = function(input) {
 			var userInput = (input.key).toLowerCase();
-			//(how do I compare the last index of one array to each index of another array; if last index of hangmanGame.userGuesses === any index of hangmanGame.letters, continue. Else, display character message)
-			
-			if (hangmanGame.lives > 0) {
-				hangmanGame.userGuesses.push(userInput);
-				console.log(hangmanGame.userGuesses);
-				console.log(guessStr);
-			}
-			
-		}
+				if (hangmanGame.lives > 0) {
+					hangmanGame.userGuesses.push(userInput);
+					console.log(hangmanGame.userGuesses);
+					var guessStr = hangmanGame.userGuesses.length;
+					console.log(guessStr);
+					console.log(userG);
+
+				}
+			};
+		
 });
 
-//if lastGuess === any index of gameword, write letter to word reveal 
-//if not in game word, write to used letters area
+//if lettersGuessed(last character) === any index of gameword, write letter to word reveal in appropriate position
+
+		//correct guesses, add letters
+
+//if not in game word and not in used letters area, write to used letters area and --		lives
+
+		//incorrect guesses add letters
+
 //else do nothing
-
-
-
-			//correct guesses
-			//$("answers-reveal").append("")
-			//$( "html" ).keyup(function() {
-			//	for (i = 0; i < gameWord.length; i++) {
-			//		var letterBox = $("<div class='letter-box'></div>");
-			//		console.log(gameWord[i]);
-
 		
 
-			//incorrect guesses
-			//$("used-letters").append("")
 
-			
-
-
-				
-			//else if already guessed (compare to letters in guessed word area), do nothing
-				//creat a temporary variable...change the index value?
-				//max length is the length of the alphabet?
-
-
-		//for each win, ++wins
-
-		//loop until answers reveal length == gameWord length or lives <1
+		//loop until answers-reveal length == gameWord length or lives <1
 
 		//display win or lose message
+
+		//for each win, ++wins
 
 		//change image
 
 		//play song (add play/pause toggle)
 
-		//reset game (clear user array, clear gameword and used letters div, select a new random gameword, display underlined boxes to gameword area)
+		//reset game (clear user array, clear gameword and used letters divs, select a new random gameword, display underlined boxes to gameword area)
 
 
 
