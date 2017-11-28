63a57322f283922da5df4bfdedb38c07fb935b61
wbwelch <wbwelch@icloud.com>
2017-11-28 10:00:55 -0600

    JS work

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,104 +1,129 @@
 // JavaScript Document
 
 //create object with letter, lives, answer arrays, wins, message
-
-var hangmanGame = {
-	letters: ["abcdefghijklmnopqrstuvwxyz"],
-	lives: 12,
-	answers: [ "Freddy", "Jason", "Michael", "Leatherface", "Candyman", "Hannibal", "Norman", "Chucky", "Pennywise", "Cujo", "Pinhead" ],
-	wins: 0,
-	messages: {
-            win: 'Winner, winner, chicken dinner!',
-            lose: 'Death!',
-            guessed: ' already guessed, please try again...',
-            validLetter: 'Please enter a letter from A-Z'
-		  },
-		};
-		
-
-	//generate random word
-	var gameWord = hangmanGame.answers[Math.floor(Math.random() * hangmanGame.answers.length)];
-
-	//console tests
-	console.log("test")
-	console.log(gameWord),
-	console.log(gameWord.length);	
-	console.log(hangmanGame.wins);
-	console.log(hangmanGame.lives);
+$(document).ready(function() {
 	
+	var hangmanGame = {
+		letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+		lives: 12,
+		wins: 0,
+		userGuesses: [],
+		answers: [ "Freddy", "Jason", "Michael", "Leatherface", "Candyman", "Hannibal", "Norman", "Chucky", "Pennywise", "Cujo", "Pinhead" ],
+		messages: {
+				win: 'Winner, winner, chicken dinner!',
+				lose: 'Death!',
+				character: 'Please enter a valid letter'
+			  },
 
-//Why isn't the text jquery working?
-
-$("#guess-remain").text(hangmanGame.lives);
-$("#wins-display").text(hangmanGame.wins);
 
 
-	
-//create game function
+
+	};
+
+		//generate random word
+		var gameWord = hangmanGame.answers[Math.floor(Math.random() * hangmanGame.answers.length)];
+	 	gameWord.split()
+
+
+
+		//console tests
+		console.log("test");
+		console.log(gameWord);
+		console.log(gameWord.length);	
+		console.log(hangmanGame.wins);
+		console.log(hangmanGame.lives);
+
+		var wins = hangmanGame.wins
+		var lives = hangmanGame.lives
+
+		$("#wins-display").append("<p>" + wins + "</p>");
+		$("#guess-remain").html("<p>" + lives + "</p>");
 
 
 		//generate underlines the length of gameWord
-		//why did this stop working?
-		for (i = 0; i < gameWord.length; i++) {
-			var letterBox = $("<div>");
-			console.log(gameWord[i]);
-			letterBox.addClass("letter-box");
-			letterBox.text(" ");
-			$("#answer-reveal").append(letterBox);
-			
-		//correct guesses
-		//$("answers-reveal").append("")
+		$("html").one("keyup", function() {
+			for (i = 0; i < gameWord.length; i++) {
+				var letterBox = $("<div class='letter-box'></div>");
+				console.log(gameWord[i]);
+				letterBox.text(gameWord[i]);
+				$("#answer-reveal").append(letterBox);
+				$(".letter-box").css('color', 'rgba(77, 72, 72, 0');
 
-		//incorrect guesses
-		//$("used-letters").append("")
-
-		//how do i compare user input to a value of a property in an object
-			//need .toLowerCase user input for accuracy
-			for (k = 0; k < gameWord.length; k++) {
-				//if onkeydown === gameWord[k]){
-					//create div in #answer-reveal
-					//add class
-					//add text
-					//append
-				//can i add text to the existing created divs with the bottom borders?
-				
-				
-
-				//else if onkeydown is !== letters && onkeydown !==gameWord 
-					//create div in #used-letters
-					//add class
-					//add text
-					//append
-					//lives--
-				
-				//else if already guessed (compare to letters in guessed word area), do nothing
-					//creat a temporary variable...change the index value?
-					//max length is the length of the alphabet?
-					
-			};
-
-	//for each win, ++wins
+			}
+		});
 	
-	//loop until user var value == gameWord value or lives value <1
-			
-	//display message
+	//(why is guessString = 0)
 
-	//change image
-	
-	//play song
+
+	var guessStr = hangmanGame.userGuesses.length;
+	var lastGuess = guessStr.length - 1;
+	console.log(lastGuess);
+
+
+
+	//loop through letters to verify valid entry
+
+		document.onkeyup = function(input) {
+			var userInput = (input.key).toLowerCase();
+			//(how do I compare the last index of one array to each index of another array; if last index of hangmanGame.userGuesses === any index of hangmanGame.letters, continue. Else, display character message)
+			
+			if (hangmanGame.lives > 0) {
+				hangmanGame.userGuesses.push(userInput);
+				console.log(hangmanGame.userGuesses);
+				console.log(guessStr);
+			}
+			
+		}
+});
+
+//if lastGuess === any index of gameword, write letter to word reveal 
+//if not in game word, write to used letters area
+//else do nothing
+
+
+
+			//correct guesses
+			//$("answers-reveal").append("")
+			//$( "html" ).keyup(function() {
+			//	for (i = 0; i < gameWord.length; i++) {
+			//		var letterBox = $("<div class='letter-box'></div>");
+			//		console.log(gameWord[i]);
+
 		
-	//reset game(loop)
-			
-	//initiate game on keypress(call function)
+
+			//incorrect guesses
+			//$("used-letters").append("")
 
 			
 
 
+				
+			//else if already guessed (compare to letters in guessed word area), do nothing
+				//creat a temporary variable...change the index value?
+				//max length is the length of the alphabet?
 
 
-	
+		//for each win, ++wins
 
-	
+		//loop until answers reveal length == gameWord length or lives <1
+
+		//display win or lose message
+
+		//change image
+
+		//play song (add play/pause toggle)
+
+		//reset game (clear user array, clear gameword and used letters div, select a new random gameword, display underlined boxes to gameword area)
 
 
-	
+
+
+
+
+
+
+
+
+
+
+
